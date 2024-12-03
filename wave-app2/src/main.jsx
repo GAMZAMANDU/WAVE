726a7732c622f0fb408bb551e0fa070a9b3ef098
gamzamandu <galbimandu1126@gmail.com>
2024-12-03 14:51:51 +0900

    fix : vite 오류 해결

--- wave-app2/src/main.jsx
+++ wave-app2/src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.js'
+import App from './App.jsx' // 여기서 .js를 .jsx로 변경
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
