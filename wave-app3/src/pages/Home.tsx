605c041c06ff5cc94b264775364b6925a7bdb864
gamzamandu <galbimandu1126@gmail.com>
2024-12-23 03:12:39 +0900

    add : 비로그인 시 login으로 이동

--- wave-app3/src/pages/Home.tsx
+++ wave-app3/src/pages/Home.tsx
@@ -1,9 +1,17 @@
+import { useEffect } from 'react';
 import Display from '@components/Display';
 import RunButton from '@components/RunButton';
 import SettingButton from '@components/SettingButton';
 import H1 from '@components/H1';
 
 const Home = () => {
+  useEffect(() => {
+    const token = localStorage.getItem('access_token');
+    if (!token) {
+      window.location.href = '/login';
+    }
+  }, []);
+
   return (
     <div className="flex flex-col items-center">
       <H1 content='Hello, world'/>
