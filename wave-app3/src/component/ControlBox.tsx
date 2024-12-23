7b32c0e170f8b2f83b49955b842c083b7d5cf53f
gamzamandu <galbimandu1126@gmail.com>
2024-12-23 15:10:33 +0900

    chore : ControlBox 컴포넌트가 id를 기준으로 icon이 적용되도록 수정, material-icon -> material-symbols-outlined으로 수정

--- wave-app3/src/component/ControlBox.tsx
+++ wave-app3/src/component/ControlBox.tsx
@@ -1,8 +1,10 @@
-const ControlBox = ({commend} : {commend : string}) => {
+import handlerContent from '../config/handlerContent.json';
+
+const ControlBox = ({id, choice = false} : {id : number, choice?: boolean}) => {
 	return (
 		<div className="flex items-center w-full gap-1 px-4 py-2 mb-2 bg-white cursor-pointer max-h-8 rounded-xl">
-			<i className='select-none material-icons'>close</i>
-			<p className='text-xs font-medium select-none'>{commend}</p>
+			<i className='select-none material-symbols-outlined'>{handlerContent[id].icon}</i>
+			<p className='text-xs font-medium select-none'>{handlerContent[id].name}</p>
 		</div>
 	);
 };
