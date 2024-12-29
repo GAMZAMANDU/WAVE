f57e1be2dc07fb99a28df2e9faa52f4d7632405a
gamzamandu <galbimandu1126@gmail.com>
2024-12-29 13:40:38 +0900

    add : extensions front 제작

--- wave-Extensions/popup.js
+++ wave-Extensions/popup.js
@@ -1,1 +1,27 @@
-console.log('This is a popup!');
+let numbers = []; 
+const numberList = document.getElementById('numberList');
+
+document.addEventListener('keydown', function (event) {
+  if (event.key >= '0' && event.key <= '9') {
+    if (numbers.length < 6) {
+      numbers.push(event.key);
+    }
+  } else if (event.key === 'Backspace') {
+    numbers.pop();
+  }else if (event.key === 'Enter') {
+    numbers.pop();
+  } else {
+    event.preventDefault();
+  }
+
+  renderNumbers();
+});
+
+function renderNumbers() {
+  numberList.innerHTML = ''; 
+  for (let i = 0; i < 6; i++) {
+    const li = document.createElement('li');
+    li.textContent = numbers[i] || '';
+    numberList.appendChild(li);
+  }
+}
